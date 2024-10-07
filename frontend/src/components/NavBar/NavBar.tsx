412b5dd68c6fe2612edadb0f0fe8fb3ba2d0a7d3
Cameron Thomas <camthomas47@gmail.com>
2024-10-07 13:29:14 -0400

    exercises UI updated, Navbar UI updated

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -2,27 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { persistor } from '../../store';
-import {
-    selectCurrentUser,
-    userLoggedOut
-} from '../../slices/usersSlice'
-
-
+import { selectCurrentUser,userLoggedOut } from '../../slices/usersSlice'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    NavItem,
-} from "reactstrap";
-
-import { Nav } from "react-bootstrap";
-import useToggle from "../../hooks/useToggle/useToggle";
+import './NavBar.css'
+import './navIcon.css'
 
 const NavBar = (): React.JSX.Element => {
     const dispatch = useAppDispatch();
-    const [collapsed, toggleCollapse] = useToggle(true);
     const currentUser = useAppSelector(selectCurrentUser);
 
     const handleLogOut = () => {
@@ -31,72 +17,46 @@ const NavBar = (): React.JSX.Element => {
         persistor.purge();
     }
 
-    const loggedInNavComponents = (
-        <div className="d-flex flex-column align-items-center">
-            <NavItem>
-                <Nav.Link as={Link} to="/account" onClick={toggleCollapse}>Account</Nav.Link>
-            </NavItem>
-            <NavItem>
-                <Nav.Link as={Link} to="/exercises" onClick={toggleCollapse}>Exercises</Nav.Link>
-            </NavItem>
-
-            <NavItem>
-                <Nav.Link as={Link} to="/workouts" onClick={toggleCollapse}>Workouts</Nav.Link>
-            </NavItem>
-            <NavItem>
-                <Nav.Link onClick={handleLogOut} as={Link} to="/">Sign Out</Nav.Link>
-            </NavItem>
-        </div> 
-    )
-
-    const nonloggedInNavComponents = (
-        <div className="d-flex flex-column align-items-center">
-            <NavItem>
-                <Nav.Link as={Link} to="/" onClick={toggleCollapse}>Login</Nav.Link>
-            </NavItem>
-            <NavItem>
-                <Nav.Link as={Link} to="/register" onClick={toggleCollapse}>Signup</Nav.Link>
-            </NavItem>
-        </div>
-
-    )
-
-
+    const handleToggle = () => {
+        const NavBarBody = document.querySelector(".NavBarBody");
+        const NavIcon = document.querySelector("#NavIcon")
+        NavIcon?.classList.toggle('open');
+        NavBarBody?.classList.toggle('offScreen');
+        NavBarBody?.classList.toggle('visible');
+    }
+    
     return  (currentUser !== null )
-    ?   <div>
-            <Navbar color="light" >
-                <div className="d-flex justify-content-between flex-grow-1">
-                    <NavbarBrand tag="a" href="/" className="fs-2 me-5">
-                        Fitly
-                    </NavbarBrand>
-                    <div >
-                        <NavbarToggler type="button" onClick={toggleCollapse} className="me-5 d-flex flex-column flex-grow-1"/>
-                        <Collapse isOpen={!collapsed} navbar>
-                            <Nav>
-                                {loggedInNavComponents} 
-                            </Nav>  
-                        </Collapse>
+    ?   <div className="NavBarContainer">
+                <div className="NavBarHeader">
+                    <div id="NavIcon" onClick={handleToggle}>
+                        <span className="toggleContent"/>
+                        <span className="toggleContent"/>
+                        <span className="toggleContent"/>
                     </div>
+                    <Link className="NavBarLink" to="/">
+                        <h4>Fitly</h4>
+                    </Link>
                 </div>
-            </Navbar>
+                    <div className="NavBarBody offScreen">
+                        <Link className="NavBarLink"  to="/account" onClick={handleToggle}>Account</Link>    
+                        <Link className="NavBarLink"  to="/exercises" onClick={handleToggle}>Exercises</Link>    
+                        <Link className="NavBarLink"  to="/workouts" onClick={handleToggle}>Workouts</Link>    
+                        <Link className="NavBarLink" onClick={handleLogOut}  to="/">Sign Out</Link>
+                    </div>
         </div>
 
     : <div>
-            <Navbar color="light" >
-                <div className="d-flex justify-content-between flex-grow-1">
-                    <NavbarBrand tag="a" href="/" className="fs-2 me-5">
-                        Fitly
-                    </NavbarBrand>
-                    <div className="d-flex flex-column align-items-center">
-                        <NavbarToggler type="button" onClick={toggleCollapse} className="me-5 d-flex flex-column flex-grow-1"/>
-                        <Collapse isOpen={!collapsed} navbar>
-                            <Nav>
-                                {nonloggedInNavComponents}
-                            </Nav>
-                        </Collapse>
+            <div className="NavBarContainer">
+                <div className="NavBarHeader">
+                    <Link className="NavBarLink" to="/">
+                        <h4>Fitly</h4>
+                    </Link>
+                    <div className="NavBarBody hidden">
+                        <Link className="NavBarLink"  to="/" onClick={handleToggle}>Login</Link>    
+                        <Link className="NavBarLink"  to="/register" onClick={handleToggle}>Signup</Link> 
                     </div>
                 </div>
-            </Navbar>
+            </div>
         </div>
 
 }
